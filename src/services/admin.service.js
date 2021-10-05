05c6296ad1317405cb49340819d34e5839a5c585
arthurzuk <arthurzuk@hotmail.com>
2021-10-05 07:57:03 -0300

    Front-end admin

--- src/services/admin.service.js
+++ src/services/admin.service.js
@@ -1,14 +1,32 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'http://localhost:8080/api/termo';
+const API_URL = 'http://localhost:8080/api/';
 
 //código para conectar com o backend
 
 class AdminService {
-  
+  async backup() {
+    return axios
+      .post(API_URL + 'backup', {
 
-  
+      })
+      .then((response) => {
+        return response;
+      });
+  }
+
+  async sendMail() {
+    return axios
+      .post(API_URL + 'sendMail', {
+
+      })
+      .then((response) => {
+        return response;
+      });
+  }
+
+
 
 }
 
