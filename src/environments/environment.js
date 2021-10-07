db3fe091f18e467cefc8e1a8853e2564d20d5c32
lukaszrafaelb <lukinhahs777@gmail.com>
2021-10-06 23:46:49 -0300

    corrige link api
    Colocando o link da api local, havia colocado a do ambiente de prod

--- src/environments/environment.js
+++ src/environments/environment.js
@@ -1,4 +1,4 @@
 export const environment = {
-    API_URL: 'https://8080-pink-meadowlark-1ttvujjs.ws-us18.gitpod.io/api/',
+    API_URL: 'http://localhost:8080/api',
     API_TERMO_URL: 'http://localhost:8080/api/termo'
 }
