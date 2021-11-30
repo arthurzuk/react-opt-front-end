7fc6fab35457e9362f7d04df145f2bc972c33053
Rodrigo Ando <hideaki.a.rodrigo@gmail.com>
2021-11-30 18:52:43 -0300

    Tentativa de notificacao de novo termo

--- src/components/Sidebar/MenuPrincipal.js
+++ src/components/Sidebar/MenuPrincipal.js
@@ -2,13 +2,19 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import AuthService from '../../services/auth.service.js';
+import userService from '../../services/user.service.js';
+import { AiFillBell } from "react-icons/ai";
+import { IconContext } from 'react-icons';
 
 export default function Menu() {
   var login = AuthService.authUser();
   var admin = null;
+  var not = {status:null};
   if (login) {
     admin = login.autorizacao === 'ROLE_ADMIN';
+    not = userService.getNotify();
   }
 
   return (
@@ -46,7 +52,24 @@ export default function Menu() {
                 </Nav.Link>
               </Nav>
             )}
-
+            {not.status === false && (
+            <Nav>
+              <IconContext.Provider value={{color: 'red'}}>
+                <NavDropdown title={<AiFillBell/>} id="nav-dropdown">
+                  <NavDropdown.Item class="text-danger" eventKey="Termo" href="/seguranca">Novo Termo!</NavDropdown.Item>
+                </NavDropdown>
+              </IconContext.Provider>            
+            </Nav>
+            )}
+            {not.status === true && (
+            <Nav>
+              <IconContext.Provider value={{ color: 'green'}}>
+                <NavDropdown title={<AiFillBell/>} id="nav-dropdown">
+                  <NavDropdown.Item class="text-success" eventKey="Termo" href="/seguranca">Termo Atualizado!</NavDropdown.Item>
+                </NavDropdown>
+              </IconContext.Provider>            
+            </Nav>
+            )}
             {login === true && (
               <Nav>
                 <Nav.Link eventKey={2} href="/login">
