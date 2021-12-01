2bbeccccd87ea320cf8d2226a71e5c4cb262024d
jeff@macbook <mfljefferson@gmail.com>
2021-11-30 22:09:43 -0300

    fix url

--- src/environments/environment.js
+++ src/environments/environment.js
@@ -1,7 +1,7 @@
 const debug = false;
 
-const url_prod = 'http://gymarker-env.eba-p8gqs9cs.sa-east-1.elasticbeanstalk.com/';
-const url_dev = 'http://localhost:5001/api/'
+const url_prod = 'http://gymarker-env.eba-p8gqs9cs.sa-east-1.elasticbeanstalk.com/api/';
+const url_dev = 'http://localhost:5000/api/'
 export const environment = {
     API_URL: debug ? url_dev : url_prod,
 }
