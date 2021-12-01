2c95e9edb5bed097e772552ae3b5d3a7ee14092b
jeff@macbook <mfljefferson@gmail.com>
2021-12-01 11:51:01 -0300

    fix env

--- src/environments/environment.js
+++ src/environments/environment.js
@@ -1,4 +1,7 @@
+const debug = false;
+
+const url_prod = 'http://gymarker-env.eba-p8gqs9cs.sa-east-1.elasticbeanstalk.com/api/';
+const url_dev = 'http://localhost:5000/api/'
 export const environment = {
-    API_URL: 'http://localhost:8080/api/',
-    API_TERMO_URL: 'http://localhost:8080/api/termo'
+    API_URL: debug ? url_dev : url_prod,
 }
